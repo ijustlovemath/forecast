9bd9bfe9f752c780686e4281a3cada4021e54c24
Jeremy <jcdejournett@gmail.com>
2020-07-30 11:04:43 -0400

    forecast script initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Installing
+
+1. Create a venv:
+
+    mkdir venv && python -m virtualenv venv
+    source venv/bin/activate
+    
+2. Install dependencies:
+
+    pip install -r requirements.txt
+
+# Using
+
+1. Run the script:
+
+    python forecast.py max patch, nc
+
+2. Click the link output to the terminal to see the NWS forecast for that location.
---
+++ forecast.py
@@ -0,0 +1,19 @@
+import sys
+
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="ijustlovemath forecast app")
+
+location = lambda:0
+location.latitude = 0.0
+location.longitude = 0.0
+
+name = ' '.join(arg for arg in sys.argv[1:])
+location = geolocator.geocode(name)
+if location is None:
+    print("please provide a location in the arguments")
+    quit()
+
+url_base = "https://forecast.weather.gov/MapClick.php?lat={:.4f}&lon={:.4f}&unit=0&lg=english&FcstType=graphical"
+url = url_base.format(location.latitude, location.longitude)
+
+print("Forecast is here: " + url)
